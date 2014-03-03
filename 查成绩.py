8f2a9ff98e2ace4a9ecb4b0378f2783f3b354156
sodas <ylefb15@gmail.com>
2014-03-03 20:26:48 +0800

    the first version
    it's not friendly for users temporarily,i will improve soon

---
+++ 查成绩.py
@@ -0,0 +1,44 @@
+#encoding: utf-8
+"""
+Copyright (C) 2011 Chen Yukang <moorekang@gmail.com>
+is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+ """
+
+import urllib,urllib2,cookielib,webbrowser
+
+#配置信息
+class uestc:
+    ID = "学号"
+    password = "密码"
+    login_path = "https://uis.uestc.edu.cn/amserver/UI/Login?goto=http%3A%2F%2Fportal.uestc.edu.cn%2Flogin.portal"
+
+    def __init__(self):
+        self.cj = cookielib.LWPCookieJar()
+        self.opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(self.cj))
+        urllib2.install_opener(self.opener)
+        self.opener.addheaders = [('User-agent','Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36')]
+
+    #模拟登陆
+    def login(self):
+        post_data = urllib.urlencode({
+            'IDToken1':self.ID,
+            'IDToken2':self.password,
+            })
+        #发起HTTP请求
+        #print post_data
+        request = urllib2.Request(self.login_path,post_data)
+        html = self.opener.open(request).read()
+        get_url = self.opener.open(request).geturl()
+        if get_url == 'http://portal.uestc.edu.cn/index.portal':
+            print get_url
+            self.cj.save('uestc.cookie')
+            print 'Login success !'
+            
+            path='http://eams.uestc.edu.cn/eams/teach/grade/course/person.action'
+            webbrowser.open(path)
+
+test=uestc()
+test.login()
